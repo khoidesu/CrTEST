8c5e1c35c3a3ce5d7a39ae9cf1f8ec6ccdcd132d
anhkhoi <109712876+khoidesu@users.noreply.github.com>
2023-12-18 22:37:26 +0700

    Update generator.cpp

--- generator.cpp
+++ generator.cpp
@@ -1,12 +1,18 @@
 #include <bits/stdc++.h>
-#include <windows.h>
+#include <fstream>
+#include <cstdlib>
+#include <cstdio>
+#include <filesystem>
 using namespace std;
-const string NAME = "Task";
+const string NAME = "TASK";
 
 int correct_cnt = 0;
 int wrong_cnt = 0;
 
-const int NTEST = 10; 
+// 1: so sanh 2 code
+// 2: tao file sinh test
+const int type = 1;
+const int NTEST = 200; 
 const int N = 1e5 + 1;
 
 mt19937 rd(chrono::steady_clock::now().time_since_epoch().count());
@@ -23,11 +29,21 @@ typedef pair<int,int>     pii;
 typedef vector<int>       vi;
 typedef vector<pii>       vii;
 
+void withoutcommand(const string &command) {
+    int err = system(command.c_str());
+}
+
+void execute(const string &command) {
+    cerr << "Executing command [" + command +  "]... "<< endl;
+    int err = system(command.c_str());
+}
+
 long long Rand(long long l, long long h) {
     assert(l <= h);
     return abs((l + rd() * 1LL * rd() % (h - l + 1)));
 }
 
+/*=Random Function==========================================================*/
 bool RandAD(void) {
     long long l = 1;
     long long h = 1000;
@@ -37,14 +53,24 @@ bool RandAD(void) {
         return 0;
     return 1;
 }
+string RandomString(int ch)
+{
+    char alpha[26] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+                          'h', 'i', 'j', 'k', 'l', 'm', 'n',
+                          'o', 'p', 'q', 'r', 's', 't', 'u',
+                          'v', 'w', 'x', 'y', 'z' };
+    string result = "";
+    for (int i = 0; i<ch; i++)
+        result = result + alpha[rand() % 26];
 
+    return result;
+}
 vi  random_array(int n,int L,int R) {   //random array of size n in range [L,R]
     vi  a(n);
     for(int i = 0 ; i < n ; ++i)
         a[i] = Rand(L,R);
     return  a;
 }
-
 vi  random_permutation(int n) {   //random permutation of size n
     vi  a(n);
     for(int i = 0 ; i < n ; ++i)
@@ -59,8 +85,7 @@ vii random_tree(int n) {   //random tree with n vertices
         E.pb(pii(i,Rand(1,i - 1)));
     for(int i = 0 ; i < n - 1 ; ++i)    {
         pii &e = E[i];
-        e.fi = x[e.fi - 1];
-        e.se = x[e.se - 1];
+        e.fi = x[e.fi - 1]; e.se = x[e.se - 1];
     }
     return  E;
 }
@@ -76,10 +101,8 @@ vii random_graph(int n) {   //random graph with n vertices
     while(m--)  {
         int x, y;
         while(1)    {
-            y = Rand(1,n);
-            x = Rand(1,y - 1);
-            if(!mp.count(pii(x,y)))
-                break;
+            y = Rand(1,n); x = Rand(1,y - 1);
+            if(!mp.count(pii(x,y)))    break;
         }
         E.pb(pii(x,y));
         mp[pii(x,y)]++;
@@ -88,47 +111,80 @@ vii random_graph(int n) {   //random graph with n vertices
     return  E;
 }
 
+void SINHTEST(ofstream &cout) { //Trinh sinh test
+}
 ___alisamikhailovna___()
 {
     srand(time(NULL));
+
+    if (type == 2)
+        withoutcommand("md " + NAME),
+        withoutcommand("cd " + NAME);
+
     for (int iTest = 1; iTest <= NTEST; iTest++)
     {
         ofstream inp((NAME + ".inp").c_str());
-        /*---------------------input-----------------------*/
-        
-        /*----------------------end.----------------------*/
+        SINHTEST(inp);
         inp.close();
 
-        auto TIME1 = chrono::steady_clock::now();
-        system((NAME + ".exe").c_str());
-        auto TIME2 = chrono::steady_clock::now();
-        auto DIFF1 = TIME2 - TIME1;
+        if (type == 1) {   // so sanh code trau va code chuan (hoac code trau nhung vip hon :D)
+            withoutcommand("cd.. " + NAME);
+            auto TIME1 = chrono::steady_clock::now();
+            system((NAME + ".exe").c_str());
+            auto TIME2 = chrono::steady_clock::now();
+            auto DIFF1 = TIME2 - TIME1;
 
+            auto TIME3 = chrono::steady_clock::now();
+            system((NAME + "_trau.exe").c_str());
+            auto TIME4 = chrono::steady_clock::now();
+            auto DIFF2 = TIME4 - TIME3;
 
-        auto TIME3 = chrono::steady_clock::now();
-        system((NAME + "_trau.exe").c_str());
-        auto TIME4 = chrono::steady_clock::now();
-        auto DIFF2 = TIME4 - TIME3;
-
-
-        if (system(("fc " + NAME + ".out " + NAME + ".ans").c_str()) != 0)
-        {
-            cout << "Test " << iTest << ": Wrong Answer!";
-            cout << "\t[normal: " << fixed << setprecision(3) << chrono::duration<double>(DIFF1).count() << "s,  ";
-            cout << "trau: " << fixed << setprecision(3) << chrono::duration<double>(DIFF2).count() << "s]\n";
-            wrong_cnt++;
-            // return 0;
+            if (system(("fc " + NAME + ".out " + NAME + ".ans").c_str()) != 0)
+            {
+                cout << "Test case #" << iTest << ": Wrong Answer!";
+                cout << "\t[: " << fixed << setprecision(3) << chrono::duration<double>(DIFF1).count() << "s,  ";
+                cout << "w/_out: " << fixed << setprecision(3) << chrono::duration<double>(DIFF2).count() << "s]\n";
+                wrong_cnt++;
+                return 0;
+            }
+            else {
+                cout << "Test case #" << iTest << ": Accepted!";
+                cout << "  [" << fixed << setprecision(3) << chrono::duration<double>(DIFF1).count() << "s,  ";
+                cout << "w/_out: " << fixed << setprecision(3) << chrono::duration<double>(DIFF2).count() << "s]\n";
+                correct_cnt++;
+            }
         }
+        else {
+            ifstream rinp((NAME + ".inp").c_str());
+            ofstream rout((NAME + ".out").c_str());
+            system((NAME + "_trau.exe").c_str());
+            rinp.close();
+            rout.close();
 
-        cout << "Test " << iTest << ": Accepted!";
-        cout << "\t[normal: " << fixed << setprecision(3) << chrono::duration<double>(DIFF1).count() << "s,  ";
-        cout << "trau: " << fixed << setprecision(3) << chrono::duration<double>(DIFF2).count() << "s]\n";
-        correct_cnt++;
+            execute("md Test" + to_string(iTest));
+            execute("copy " +NAME+ ".inp Test" + to_string(iTest));
+
+            string oldFileName = NAME + ".ans";
+            string newFileName = NAME + ".out";
+            ofstream outStream(newFileName, ios::app);
+            ifstream ansStream(oldFileName);
+            outStream << ansStream.rdbuf();
+            outStream.close();
+            ansStream.close();
+
+            execute("copy " + newFileName + " Test" + to_string(iTest));
+
+            execute("move Test" + to_string(iTest) + " " + NAME);
+        }
     }
-    cout <<"----------------------\n";
-    if (correct_cnt == NTEST)
-        cout << "AC\n";
-    else
-        cout << "\nWrong Answer: " << wrong_cnt << '\n'; 
+
+    if (type == 1) {
+        cout << "----------------------\n";
+        cout << "Total: ";
+        if (correct_cnt == NTEST)
+            cout << "AC\n";
+        else
+            cout << "Wrong Answer: " << wrong_cnt << '\n'; 
+    }
     return 0;
 }
